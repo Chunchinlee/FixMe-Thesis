3ea0df269410b1068714c28c5e39680dd8fd4e39
Chunchinlee <torresmichaellhance@gmail.com>
2024-09-22 23:48:35 +0800

    Update Signup.js

--- js/Signup.js
+++ js/Signup.js
@@ -51,27 +51,22 @@ document.getElementById('signupForm').addEventListener('submit', (e) => {
 });
 
 // Handle form submission for Log In
-login.addEventListener('click', (e) => {
+document.getElementById('Login').addEventListener('click', (e) => {
+  e.preventDefault();  // Make sure to prevent default form submission
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
 
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-
       const user = userCredential.user;
-
       const dt = new Date();
       update(ref(database, 'users/' + user.uid), {
         last_login: dt,
       })
-
       alert('User Logged in!');
-
     })
     .catch((error) => {
-      const errorCode = error.code;
       const errorMessage = error.message;
-      
       alert(errorMessage);
     });
 });

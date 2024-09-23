e39e2aacdb16b2ea97b7968cb9128255cc9eab41
Chunchinlee <torresmichaellhance@gmail.com>
2024-09-23 21:09:14 +0800

    as

--- js/Login.js
+++ js/Login.js
@@ -2,23 +2,3 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/fireba
 import { getDatabase, ref, update } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 
-// Handle form submission for Log In
-document.getElementById('Login').addEventListener('click', (e) => {
-  e.preventDefault();  // Make sure to prevent default form submission
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-
-  signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      const dt = new Date();
-      update(ref(database, 'users/' + user.uid), {
-        last_login: dt,
-      })
-      alert('User Logged in!');
-    })
-    .catch((error) => {
-      const errorMessage = error.message;
-      alert(errorMessage);
-    });
-});

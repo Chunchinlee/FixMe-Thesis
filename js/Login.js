e340e33113b7577a50ee7cd59a2954130d81b848
Chunchinlee <torresmichaellhance@gmail.com>
2024-09-25 01:39:54 +0800

    Update Login.js

--- js/Login.js
+++ js/Login.js
@@ -1,16 +1,17 @@
+// Import the functions you need from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 import { getDatabase, ref, update } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBJ0PlYIIMJOHh2xueY5XdG4hdLu9aQAu0",
-    authDomain: "fixme-a1b21.firebaseapp.com",
-    databaseURL: "https://fixme-a1b21-default-rtdb.firebaseio.com",
-    projectId: "fixme-a1b21",
-    storageBucket: "fixme-a1b21.appspot.com",
-    messagingSenderId: "522121216989",
-    appId: "1:522121216989:web:4834570c0400d50e856c34"
+  apiKey: "AIzaSyBJ0PlYIIMJOHh2xueY5XdG4hdLu9aQAu0",
+  authDomain: "fixme-a1b21.firebaseapp.com",
+  databaseURL: "https://fixme-a1b21-default-rtdb.firebaseio.com",
+  projectId: "fixme-a1b21",
+  storageBucket: "fixme-a1b21.appspot.com",
+  messagingSenderId: "522121216989",
+  appId: "1:522121216989:web:4834570c0400d50e856c34"
 };
 
 // Initialize Firebase
@@ -20,24 +21,23 @@ const provider = new GoogleAuthProvider();
 
 const signInButton = document.getElementById("signInButton");
 
-const userSignIn = async () => {
-    try {
-        const result = await signInWithPopup(auth, provider);
-        const user = result.user;
-        console.log(user);
-    } catch (error) {
-        console.error('Error during sign in:', error.message);
-    }
-};
-
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        alert("You have Signed in!");
-         window.location.href = 'Student.html';
-    } else {
-        // Handle signed out state if needed
-    }
-});
-
+const userSignIn = async() =>{
+  signInWithPopup(auth, provider)
+  .then((result) =>{
+    const user = result.user
+    console.log(user);
+  }).catch((error) =>{
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  }) 
+}
+onAuthStateChanged(auth, (user) =>{
+  if(user) {
+    alert("You have Signed in!");
+  }else{
+  }
+})
 signInButton.addEventListener('click', userSignIn);
-signOutButton.addEventListener('click', userSignIn)
+
+
+

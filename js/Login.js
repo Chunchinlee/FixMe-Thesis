d7c890fa880d2a111fcc662e42aac4e593b8eedf
Chunchinlee <torresmichaellhance@gmail.com>
2024-09-27 05:11:56 +0800

    a

--- js/Login.js
+++ js/Login.js
@@ -1,11 +1,13 @@
 // Import the functions you need from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getDatabase, ref, update } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 
 // Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBJ0PlYIIMJOHh2xueY5XdG4hdLu9aQAu0",
   authDomain: "fixme-a1b21.firebaseapp.com",
+  databaseURL: "https://fixme-a1b21-default-rtdb.firebaseio.com",
   projectId: "fixme-a1b21",
   storageBucket: "fixme-a1b21.appspot.com",
   messagingSenderId: "522121216989",
@@ -19,43 +21,33 @@ const provider = new GoogleAuthProvider();
 
 const signInButton = document.getElementById("signInButton");
 const signOutButton = document.getElementById("signOutButton");
-const message = document.getElementById("message");
 
-signOutButton.style.display = "none";
-message.style.display = "none";
-
-const userSignIn = async () => {
+const userSignIn = async() =>{
   signInWithPopup(auth, provider)
-    .then((result) => {
-      const user = result.user;
-      console.log(user);
-    })
-    .catch((error) => {
-      console.error(error.code, error.message);
-    });
-};
+  .then((result) =>{
+    const user = result.user
+    console.log(user);
+  }).catch((error) =>{
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  }) 
+}
 
-const userSignOut = async () => {
-  signOut(auth)
-    .then(() => {
-      alert("You have signed out successfully!");
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-};
+const userSigOut = async() =>{
+  signOut(auth, provider)
+  .then((result) =>{
+    alert("You have Sign out successfully!")
+  }).catch((error) =>{}) 
+}
 
-signInButton.addEventListener("click", userSignIn);
-signOutButton.addEventListener("click", userSignOut);
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    signInButton.style.display = "none";
-    signOutButton.style.display = "block";
-    message.style.display = "block";
-  } else {
-    signInButton.style.display = "block";
-    signOutButton.style.display = "none";
-    message.style.display = "none";
+onAuthStateChanged(auth, (user) =>{
+  if(user) {
+    alert("You have Signed in!");
+  }else{
   }
-});
+})
+signInButton.addEventListener('click', userSignIn);
+signOutButton.addEventListener('click', userSigOut);
+
+
+

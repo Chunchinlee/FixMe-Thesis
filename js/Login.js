619d36c60e51589a8dd3d0216c9acf65ca27224a
Chunchinlee <torresmichaellhance@gmail.com>
2024-10-01 01:38:16 +0800

    a

--- js/Login.js
+++ js/Login.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 import { getDatabase, ref, update } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 
 // Firebase configuration
@@ -17,54 +17,39 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-const provider = new GoogleAuthProvider();
+const database = getDatabase();
 
-const signInButton = document.getElementById("signInButton");
-const signOutButton = document.getElementById("signOutButton");
-const message = document.getElementById("message");
-const userName = document.getElementById("userName");
 
-signOutButton.style.display ="none";
-message.style.display = "none";
+submitData.addEventListener('click', (e) => {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
 
-const userSignIn = async () => {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      const user = result.user;
-      console.log(user);
-      console.log("Redirecting to Student.html"); // Debug log
-      window.location.href = 'Login.html';
+
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+    var lgDate = new Date();
+    update(ref(database, 'users/' + user.uid), {
+      last_login: lgDate,
     })
-    .catch((error) => {
-      console.log(error);  
-    });
-}
-
-const userSignOut = async () => {
-  signOut(auth)
-    .then(() => {
-      alert("You have signed out successfully!");
-    })
-    .catch((error) => {
-      console.log(error); 
-    });
-}
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    signOutButton.style.display = "block"; // Show sign-out button
-    message.style.display = "block";
-    userName.innerHTML = user.displayName;
-  } else {
-    signOutButton.style.display = "none"; // Hide sign-out button
-    message.style.display = "none";
-  }
+      .then(() => {
+        alert('User Logged in Successfully');
+        window.location.href = 'Student.html';
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage);
+  });
+  signOut(auth).then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
 });
-
-signInButton.addEventListener('click', userSignIn);
-signOutButton.addEventListener('click', userSignOut);
-
-
-
-
-

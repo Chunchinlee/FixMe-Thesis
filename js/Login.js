33ae44e1a5d57c200f5057ce04ab03fb32658dc4
Chunchinlee <torresmichaellhance@gmail.com>
2024-10-01 13:24:59 +0800

    as

--- js/Login.js
+++ js/Login.js
@@ -26,6 +26,18 @@ const userName = document.getElementById("userName");
 
 signOutButton.style.display ="none";
 message.style.display = "none";
+const passwordField = document.getElementById('password');
+const togglePassword = document.getElementById('togglePassword');
+
+togglePassword.addEventListener('click', function () {
+  // Toggle the type attribute
+  const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+  passwordField.setAttribute('type', type);
+  
+  // Toggle the icon
+  this.classList.toggle('fa-eye');
+  this.classList.toggle('fa-eye-slash');
+});
 
 const userSignIn = async () => {
   signInWithPopup(auth, provider)
